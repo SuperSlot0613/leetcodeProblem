af7dfb72989287b99109fd3788fdf3741c3a4d41
Saurabh0613 <saurabhyadav0613@gmail.com>
2022-04-12 19:23:29 +0530

    Time: 24 ms (64.88%), Space: 6.2 MB (7.61%) - LeetHub

---
+++ 326-power-of-three/326-power-of-three.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    
+      bool powerTo(int index,int n){
+        if(n==pow(3,index)){
+            return true;
+        }
+        
+        if(pow(3,index)>n){
+            return false;
+        }
+        
+        return powerTo(index+1,n);
+        
+    }
+    
+    
+    bool isPowerOfThree(int n) {
+        return powerTo(0,n);
+    }
+};
