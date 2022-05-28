3fbcf6e9f17418d84ca0c94990770cd92afd19f4
Saurabh0613 <saurabhyadav0613@gmail.com>
2022-05-28 20:26:32 +0530

    Time: 0 ms (100.00%), Space: 6.1 MB (10.19%) - LeetHub

--- 509-fibonacci-number/509-fibonacci-number.cpp
+++ 509-fibonacci-number/509-fibonacci-number.cpp
@@ -1,13 +1,22 @@
 class Solution {
 public:
+    
+    int fibTOP(int n,vector<int> &dp){
+        if(n==0 or n==1){
+            return n;
+        }
+        
+        if(dp[n]!=-1){
+            return dp[n];
+        }
+        dp[n]=fibTOP(n-1,dp)+fibTOP(n-2,dp);
+        return dp[n];
+    }
+    
+    
     int fib(int n) {
-        if(n==0){
-            return 0;
-        }
-        if(n==1){
-            return 1;
-        }
-        int sum=fib(n-1)+fib(n-2);
-        return sum;
+        vector<int> dp(n+1,-1);
+        return fibTOP(n,dp);
+       
     }
 };
