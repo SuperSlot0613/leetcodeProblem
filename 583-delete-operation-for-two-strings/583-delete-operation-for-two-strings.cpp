b3ab2182639700e664ec35f6b9edf9b4bb5302f2
Saurabh0613 <saurabhyadav0613@gmail.com>
2022-06-13 19:20:14 +0530

    Time: 45 ms (25.78%), Space: 12.5 MB (9.90%) - LeetHub

---
+++ 583-delete-operation-for-two-strings/583-delete-operation-for-two-strings.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    
+     int minEditdistanceTD(int i,int j,string &s1,string &s2,vector<vector<int>>&dp){
+        if(i<0) return j+1;
+        if(j<0) return i+1;
+        if(dp[i][j]!=-1) return dp[i][j];
+        if(s1[i]==s2[j]){
+            return dp[i][j]=minEditdistanceTD(i-1,j-1,s1,s2,dp);
+        }
+        dp[i][j]=min({1+minEditdistanceTD(i-1,j,s1,s2,dp),1+minEditdistanceTD(i,j-1,s1,s2,dp)});
+        return dp[i][j];
+    }
+    
+    int minDistance(string s1, string s2) {
+        int n=s1.size();
+        int m=s2.size();
+        vector<vector<int>> dp(n,vector<int>(m,-1));
+        return minEditdistanceTD(n-1,m-1,s1,s2,dp);
+    }
+};
