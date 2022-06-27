01f620e20754226c5f6250cba147846113325cf2
Saurabh0613 <saurabhyadav0613@gmail.com>
2022-06-27 22:11:22 +0530

    Time: 32 ms (20.34%), Space: 19.8 MB (44.36%) - LeetHub

--- 42-trapping-rain-water/42-trapping-rain-water.cpp
+++ 42-trapping-rain-water/42-trapping-rain-water.cpp
@@ -24,4 +24,26 @@ public:
         }
         return res;
     }
+    
+    int waterTrapped(vector<int>&v){
+        int watretraped=0;
+        int n=v.size();
+        for(int i=0;i<v.size();i++){
+            int j=i;
+            int leftmax=0,rightmax=0;
+            while(j>=0){
+                leftmax=max(leftmax,v[i]);
+                j--;
+            }
+            j=i;
+            while(j<n){
+                rightmax=max(rightmax,v[i]);
+                j++;
+            }
+            
+            watretraped+=min(leftmax,rightmax)-v[i];
+        }
+        return watretraped;
+    }
+    
 };
