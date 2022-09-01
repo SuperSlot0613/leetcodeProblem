ae9ebdd98069ed55762634988558406eea09ed5d
Saurabh0613 <saurabhyadav0613@gmail.com>
2022-09-01 18:39:11 +0530

    Time: 225 ms (37.51%), Space: 86.4 MB (9.98%) - LeetHub

--- 1448-count-good-nodes-in-binary-tree/1448-count-good-nodes-in-binary-tree.cpp
+++ 1448-count-good-nodes-in-binary-tree/1448-count-good-nodes-in-binary-tree.cpp
@@ -12,22 +12,6 @@
 class Solution {
 public:
     
-//     int count = 0;
-//     void dfs(TreeNode* node,int curMax){
-//         if (!node) return;
-        
-//         if (node->val >= curMax){
-//             count++;
-//             curMax = node->val;
-//         }
-//         dfs(node->left, curMax);
-//         dfs(node->right, curMax);
-//     }
-//     int goodNodes(TreeNode* root) {
-//         dfs(root, root->val);
-//         return count;
-//     }
-    
     int count=0;
     
     void preorder(TreeNode* root,int maxval){
