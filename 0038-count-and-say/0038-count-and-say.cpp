7fcbd104868c1e12c4723778e1e9e57deb5e4dda
Saurabh0613 <saurabhyadav0613@gmail.com>
2022-10-15 16:07:36 +0530

    Time: 114 ms (13.21%), Space: 120.9 MB (20.61%) - LeetHub

---
+++ 0038-count-and-say/0038-count-and-say.cpp
@@ -0,0 +1,37 @@
+class Solution {
+public:
+    
+    string test(string str){
+        int i=1, count=1;
+        char prev=str[0];
+        string ans="";
+        while(i<str.size()){
+            if(str[i]==prev){
+                count++;
+            }else{
+                // cout<<count<<" "<<prev<<endl;
+                ans=ans+to_string(count);
+                ans+=prev;
+                prev=str[i];
+                count=1;
+            }
+            i++;
+        }
+        ans+=to_string(count)+prev;
+        return ans;
+    }
+    
+    string countAndSay(int n) {
+        if(n==1){
+            return "1";
+        }
+        string str="1";
+        for(int i=1;i<n;i++){
+            string temp=test(str);
+            // cout<<temp<<endl;
+            str=temp;
+            // cout<<str<<endl;
+        }
+        return str;
+    }
+};
